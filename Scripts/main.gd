742d97ce6826e9e4c275ffb7211e40c2c002a440
olso9261 <olso9261@umn.edu>
2023-09-29 15:39:40 -0500

    Very Basic Mining

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var tilemap
 var map
 var map_width := 18
 var map_height := 10
@@ -20,16 +21,27 @@ func init_map(x:int, y:int):
 		
 	return map
 
+func mine_tile():
+	var player = get_node("Player")
+	var nearby = tilemap.get_surrounding_cells(tilemap.local_to_map(player.get("position")))
+	for i in nearby:
+		if (tilemap.get_cell_atlas_coords(0, i) == Vector2i(16,8)):
+			tilemap.set_cell(0, i, 0, Vector2i(0, 2), 0)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	map = init_map(17, 9)
-	var tilemap = get_node("TileMap");
+	#map = init_map(17, 9)
+	tilemap = get_node("TileMap");
 	
 	for w in range(map_width):
 		for h in range(map_height):
-			tilemap.set_cell(0, Vector2i(w, h), 0, Vector2i(0, 10), 0)
+			tilemap.set_cell(0, Vector2i(w, h), 0, Vector2i(16, 8), 0) #dirt blocks
+	for w in range(4, 8):
+		for h in range(4, 8):
+			tilemap.set_cell(0, Vector2i(w,h), 0, Vector2i(0, 2), 0) #grass blocks
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if (Input.is_action_just_pressed("mine")):
+		mine_tile()
