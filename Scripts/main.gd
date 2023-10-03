99501ef4763fa0302245ffcb2529aa67a2fe4c36
olso9261 <olso9261@umn.edu>
2023-10-02 19:25:39 -0500

    Camera
    Added a camera to follow the player

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -4,24 +4,8 @@ extends Node2D
 var tilemap
 var player
 
-var map_width := 18
-var map_height := 11
-
-#returns a 2D array of Vector3.ZERO's X wide and Y tall
-#The vectors populating the returned map represent (i, x, y), where
-#i is the id of the tilemap to access
-#x is the x_pos of the tile in the tilemap
-#y is the y_pos of the tile in the tilemap
-func init_map(x:int, y:int):
-	var map = []
-	
-	for w in range(x):
-		var col: Array[Vector3] = []
-		col.resize(y)
-		col.fill(Vector3.ZERO)
-		map.append(col)
-		
-	return map
+var map_width := 36
+var map_height := 22
 
 
 #breaks foreground tiles
@@ -41,11 +25,14 @@ func explode_at(tile:Vector2i):
 		if (tilemap.get_cell_atlas_coords(1, t) == Tiles.DIRT):
 				tilemap.set_cell(1, t, 0, Tiles.EMPTY, -1)
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#map = init_map(17, 9)
 	player = get_node("Player")
 	tilemap = get_node("TileMap");
+	
+	player.set_position(tilemap.map_to_local(Vector2i(6,6)))
 	
 	for w in range(map_width):
 		for h in range(map_height):
